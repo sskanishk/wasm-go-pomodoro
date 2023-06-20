ddc845049bfc47b77a8932bccbc419ec23c52124
sskanishk <kanishkmalviya78@gmail.com>
2023-06-20 22:10:52 +0530

    yt

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const goWasm = new Go()
 WebAssembly.instantiateStreaming(fetch("main.wasm"), goWasm.importObject)
     .then((result) => {
         goWasm.run(result.instance)
-        console.log("Result ", result)
+
         document.getElementById("get-html").addEventListener("click", () => {
             document.body.innerHTML += getHtml()
         })
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	fmt.Println("Server started on http://localhost:9000")
-	log.Fatal(http.ListenAndServe(":9000", http.FileServer(http.Dir("../../web"))))
+	log.Fatal(http.ListenAndServe(":9000", http.FileServer(http.Dir("../../assets"))))
 }
